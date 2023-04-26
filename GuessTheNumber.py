b0d5c53de60fb428c619173d2edaa6facff38cb8
Margarita <margodevalois@gmail.com>
2023-04-26 12:07:43 +0200

    fix: change to 5 tries

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,20 +1,26 @@
-# Computer proposes a number, human guess it
+# Computer proposes a number, human guesses it, 5 tries
 import random
 
 print("\t\tWelcome to the game 'Guess the number'")
 print("I guessed a natural number from 1 to 100")
-print("Try to guess it!\n")
+print("Try to guess it! But you have only 5 tries\n")
 the_number = random.randint(1, 100)
 guess = int(input("Your proposal: "))
 tries = 1
-while guess != the_number:
+while tries < 5:
     if guess > the_number:
         print("Lower...")
-    else:
+        guess = int(input("Your proposal one more time: "))
+        tries += 1
+    elif guess < the_number:
         print("Bigger...")
-    guess = int(input("Your proposal one more time: "))
-    tries += 1
-print("You made it! It really was", the_number)
-print("You did it with", tries, "tries\n")
+        guess = int(input("Your proposal one more time: "))
+        tries += 1
+    elif guess == the_number:
+        print("\nYou made it! It really was", the_number)
+        break
+else:
+    print("\nYou died!! My number was", the_number)
 
 input("\nPush Enter, to quit")
+
